ebdf493a0f26fcd0ed1252827d96e7e781d1d8d5
Milan Janjic <milanjanjic23@yahoo.com>
2018-11-05 16:57:38 +0100

    Updates

---
+++ precache-manifest.ab6d81aaec2c2112c6ffccf880ad6ae7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "45712353402ae3e9913d",
+    "url": "/MilanJanjic/react-contactmanager/static/css/main.462756d5.chunk.css"
+  },
+  {
+    "revision": "45712353402ae3e9913d",
+    "url": "/MilanJanjic/react-contactmanager/static/js/main.45712353.chunk.js"
+  },
+  {
+    "revision": "45e93766a04030bffd97",
+    "url": "/MilanJanjic/react-contactmanager/static/css/1.9eff6d82.chunk.css"
+  },
+  {
+    "revision": "45e93766a04030bffd97",
+    "url": "/MilanJanjic/react-contactmanager/static/js/1.45e93766.chunk.js"
+  },
+  {
+    "revision": "d81c5c52820708f07456",
+    "url": "/MilanJanjic/react-contactmanager/static/js/runtime~main.d81c5c52.js"
+  },
+  {
+    "revision": "a9b596db49d77f26d5b0c684b5696b74",
+    "url": "/MilanJanjic/react-contactmanager/index.html"
+  }
+];
